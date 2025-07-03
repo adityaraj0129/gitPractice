6c5109843b5f04b878a075459ba8da53dfa8d51f
ADITYA RAJ TIWARI <adityarajtiwari2003@gmail.com>
2025-07-03 13:27:56 +0530

    Added printing sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,8 @@
 class sample{
     public static void main(String[] args) {
         System.out.println("Learning git and github");
+         System.out.println("Learning git and github");
+          System.out.println("Learning git and github");
+           System.out.println("Learning git and github");
     }
 }
